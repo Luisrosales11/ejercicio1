4a37e552a54ac36106c4964d912efee51255c670
allenvasqueez <allen_josue7@yahoo.com>
2016-05-27 09:36:38 -0600

    Update Clases.java
    Clase producto creada

--- src/Formulario/Clases.java
+++ src/Formulario/Clases.java
@@ -11,5 +11,65 @@ package Formulario;
  * @author pc
  */
 public class Clases {
+    package clases;
+
+public class producto {
+    private String nombre;
+    private String marca;
+    private Double precio;
+    private int existencia;
+
+    public producto(){
+    
+    }
+    public producto(String nombre,String marca,Double precio,int existencia){
+    this.nombre=nombre;
+    this.marca=marca;
+    this.precio=precio;
+    this.existencia=existencia;
+    
+    
+    }
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+    
+    
+    
+    
     
 }
+
+    
+}
+
